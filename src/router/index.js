cae6b4c582346d883f8300f1935f6a9fa7864b50
m-daichi1219 <matu9525@gmail.com>
2019-11-17 13:52:43 +0900

    追加　依存関係の読み込みとstoreの定義を追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'login',
+    component: () => import('../views/Login.vue'),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/chat',
+    name: 'chat',
+    component: () => import('../views/ChatDashboard.vue')
   }
 ]
 
