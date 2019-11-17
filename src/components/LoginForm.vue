d9b2b596ef7f374a99c907450d2a6be20bbab598
m-daichi1219 <matu9525@gmail.com>
2019-11-17 17:40:01 +0900

    追加　ログインフォームの実装

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login-form">
+    <h5 class="text-center">Vue.js Chatにログイン</h5>
+    <hr>
+    <!-- イベント修飾子(prevent)によってページのリロードを行わない -->
+    <b-form @submit.prevent="onSubmit">
+      <b-alert variant="danger" :show="hasError">{{ error }}</b-alert>
+
+      <!-- ログインフォーム -->
+      <b-form-group id="userInputGroup" label="ユーザー名" label-for="userInput">
+        <b-form-input
+          id="userInput"
+          type="text"
+          placeholder="ユーザー名を入力してください"
+          v-model="userId"
+          autocomplete="off"
+          :disabled="loading"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <!-- submitボタン -->
+      <b-button
+        type="submit"
+        variant="primary"
+        class="ld-ext-right"
+        :class="{ running: loading}"
+        :disabled="isValid"
+      >
+        Login
+        <div clss="ld ld-ring ld-spin"></div>
+      </b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  name: 'login-form',
+  data() {
+    return {
+      userId: ''
+    }
+  },
+  computed: {
+    isValid: function() {
+      const result = this.userId.length < 3;
+      return result ? result : this.loading
+    }
+  },
+  ...mapState([
+    'loading',
+    'error'
+  ]),
+  ...mapGetters([
+    'hasError'
+  ])
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login">
+    <b-jumbotron
+      header="Vue.js Chat"
+      lead="Chatkit SDKとBootstrap-Vueを使用したアプリケーション"
+      bg-variant="info"
+      text-variant="white"
+    >
+      <p>Chatkit SDKの詳細はウェブサイトをご覧ください</p>
+      <b-btn target="_blank" href="https://pusher.com/chatkit">詳細</b-btn>
+    </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col lg="4" md="3"></b-col>
+        <b-col lg="4" md="6">
+          <LoginForm />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
